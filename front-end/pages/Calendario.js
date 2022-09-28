f8da2cb171b253417291662621860247e7c0cbf0
Vinícius de Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2022-09-28 15:14:44 -0300

    .

--- front-end/pages/Calendario.js
+++ front-end/pages/Calendario.js
@@ -1,13 +1,15 @@
-import styles from '../styles/table.module.css'
+import styles from '../styles/calendario.module.css'
 import Navbar from "../components/navbar"
 
 export default function calendario() {
   return (
-    <div className={styles.geral}>
+    <div className={styles.allcalendario}>
    <div>
     <Navbar />
    </div>
-    
+   <div className={styles.principal}>
+   <h1>HOJE</h1>
+   </div>
 
    </div>
   )
