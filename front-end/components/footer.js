ed2cfdc3abc9dd2cd38e10be51520022d43dcb8c
Eduardo Kauã de Souza <93546837+EduardoKaua@users.noreply.github.com>
2022-10-05 15:26:43 -0300

    footer

--- front-end/components/footer.js
+++ front-end/components/footer.js
@@ -2,13 +2,15 @@ import styles from '../styles/footer.module.css'
 
 export default function footer() {
     return (
-	<div>
+	<div className={styles.all}>
   <footer className={styles.footer}>
     <p className={styles.follow}>Nos siga</p>
 	<p className={styles.contact}>Contato</p>
 	<p className={styles.number}>+55 (47) 99642-9894</p>
-	<img src="eliti.png"/>
+	<img className={styles.eliti} src="ELITI.png"/>
+  <img className={styles.insta} src="insta.png"/>
+  <img className={styles.discord} src="discord.png"/>
   </footer>
-    </div>
+  </div>
 );
 };
