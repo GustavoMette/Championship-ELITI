ead7f3748f69bed65c6248aca11b1ee1d2d48be6
Vinícius de Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2022-10-21 14:49:53 -0300

    .

--- front-end/pages/csgo/timescs.js
+++ front-end/pages/csgo/timescs.js
@@ -1,29 +1,29 @@
-import styles from '../../styles/table.module.css'
-import Link from 'next/link'
-import Footer from "../../components/footer"
-import Navbar from "../../components/navbar"
+import styles from "../../styles/table.module.css";
+import Link from "next/link";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
 
 export default function timescs() {
   return (
     <div className={styles.container}>
       <Navbar />
-    <div className={styles.teams}>
-   <p className={styles.esp}> Espartanos </p>
-   <p className={styles.red}> Reds </p>
-   <p className={styles.wak}> Wakanda </p>
-   <p className={styles.vik}> Vikings </p>
-   <p className={styles.amc}> Amcom </p>
-   <p className={styles.cal}> Calvos </p>
-   </div>
-   <div className={styles.buttons}>
-   <p className={styles.espb}>⬜</p>
-   <p className={styles.redb}>⬜</p>
-   <p className={styles.wakb}>⬜</p>
-   <p className={styles.vikb}>⬜</p>
-   <p className={styles.amcb}>⬜</p>
-   <p className={styles.calb}>⬜</p>
-   </div>
-   <Footer />
-   </div>
-  )
-    }
+      <div className={styles.teams}>
+        <p className={styles.esp}> Espartanos </p>
+        <p className={styles.red}> Reds </p>
+        <p className={styles.wak}> Wakanda </p>
+        <p className={styles.vik}> Vikings </p>
+        <p className={styles.amc}> Amcom </p>
+        <p className={styles.cal}> Calvos </p>
+      </div>
+      <div className={styles.buttons}>
+        <p className={styles.espb}>⇩</p>
+        <p className={styles.redb}>↴</p>
+        <p className={styles.wakb}>↴</p>
+        <p className={styles.vikb}>↴</p>
+        <p className={styles.amcb}>↴</p>
+        <p className={styles.calb}>↴</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
