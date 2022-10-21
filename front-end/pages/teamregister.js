a964c621866fc877da33ad77d64609cb798d4e25
Vinícius de Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2022-10-21 16:30:49 -0300

    .

---
+++ front-end/pages/teamregister.js
@@ -0,0 +1,11 @@
+import styles from '../styles/teamregister.module.css';
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+export default function teamregister () {
+    return (
+        <div>
+            
+        </div>
+    );
+}
