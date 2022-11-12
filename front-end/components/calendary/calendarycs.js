f56745dc82f2b19d76f71e095c54b9d09eef2296
Vinícius de Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2022-11-12 20:30:39 -0300

    .

---
+++ front-end/components/calendary/calendarycs.js
@@ -0,0 +1,21 @@
+import styles from '../styles/calendario.module.css'
+import Navbar from "../components/navbar"
+
+export default function calendarycs() {
+  return (
+    <div className={styles.allcalendario}>
+   <div>
+    <Navbar />
+   </div>
+
+    <h1 className={styles.hj}>Hoje</h1>
+    <h1 className={styles.ama}>Amanhã</h1>
+   
+   <p className={styles.wv}>🔴 WKD vs VKS</p>
+   <p className={styles.ca}>⚫ CAL vs AMC</p>
+   <p className={styles.wr}>⚫ WKD vs RED</p>
+   <p className={styles.ce}>⚫ CAL vs ESP</p>
+   <p className={styles.av}>⚫ AMC vs VKS</p>
+   </div>
+  )
+    }
